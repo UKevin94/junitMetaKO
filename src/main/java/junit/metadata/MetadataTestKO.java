b3a24dd1a4d82ec144b744e8baf27b2640d271ca
Kévin Ung <kung@NX-DOMAIN.LAN>
2019-07-09 14:43:21 +0200

    Changed dependency version + uncomment solo key

--- src/main/java/junit/metadata/MetadataTestKO.java
+++ src/main/java/junit/metadata/MetadataTestKO.java
@@ -8,7 +8,7 @@ public class MetadataTestKO {
 
     @Test
     @TFMetadata( key = "éàùôöîïèë", value = {"éééàùôöîïè"})
-    //@TFMetadata( key = "àààéàùôöîïè")
+    @TFMetadata( key = "àààéàùôöîïè")
     @TFMetadata( key = "ùùùùéàùôöîïè", value = {"ùùone", "ééétwo", "ààààthree"})
     public void accent (){
         int a = 2;
@@ -18,7 +18,7 @@ public class MetadataTestKO {
 
     @Test
     @TFMetadata( key = "first_K-E.Y&~#{}=+°|$£¤*%§!?;", value = {"vv&~#{}=+°|$£¤*%§!?;"})
-    //@TFMetadata( key = "22&~#{}=+°|$£¤*%§!?;")
+    @TFMetadata( key = "22&~#{}=+°|$£¤*%§!?;")
     @TFMetadata( key = "multi&~#{}=+°|$£¤*%§!?;", value = {"first&~#{}=+°|$£¤*%§!?;", "second&~#{}=+°|$£¤*%§!?;", "third&~#{}=+°|$£¤*%§!?;"})
     public void specialChar (){
         int a = 2;
@@ -27,9 +27,18 @@ public class MetadataTestKO {
     }
 
     @Test
-    @TFMetadata( key = "first_K-E.Y", value = {""})
+    @TFMetadata( key = "first_K-E.Y", value = {})
     @TFMetadata( key = "mu.l-ti_Key", value = {"first","two",})
     public void noValue (){
+        int a = 2;
+        int b = 2;
+        Assertions.assertEquals(a, b);
+    }
+
+    @Test
+    @TFMetadata( key = "same", value = {})
+    @TFMetadata( key = "same", value = {"first","two",})
+    public void sameKeyMultipleAnnotWarning (){
         int a = 2;
         int b = 2;
         Assertions.assertEquals(a, b);
